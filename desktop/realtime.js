3081ccb36e585cb433c9c2887c091f170d19da51
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2024-01-15 15:44:55 +0100

    Update realtime.js

--- desktop/realtime.js
+++ desktop/realtime.js
@@ -2,7 +2,7 @@
     // JavaScript-code om de tijd en datum in realtime bij te werken
     function updateTimeAndDate() {
         var nu = new Date();
-        var tijdEnDatumTekst = "Huidige tijd en datum: " + nu.toLocaleString();
+        var tijdEnDatumTekst = "Hello User Its" + nu.toLocaleString();
         document.getElementById("tijdEnDatum").innerHTML = tijdEnDatumTekst;
     }
 
